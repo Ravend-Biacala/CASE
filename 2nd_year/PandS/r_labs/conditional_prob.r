a9f023004a32c740fb1b0dbc1327a68e88b2b047
Dale Calabia <33296464+Ravend-Biacala@users.noreply.github.com>
2019-03-27 09:50:39 +0000

    Create conditional_prob.r

---
+++ 2nd_year/PandS/r_labs/conditional_prob.r
@@ -0,0 +1,12 @@
+ans = .40*0.01 + .25*0.02 + .35*0.15
+# q1---------------
+messages = c(.40, .25, .35)
+errors = c(.01, .02, 0.15)
+prob = messages*erros
+sum(prob)
+
+# q2 --------------
+code = c(.15,.20,.25,.40)
+errors = c(.05,.03,.02,.01)
+prob = code*errors
+sum(prob)
