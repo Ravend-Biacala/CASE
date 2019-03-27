4dcbb1f94e6d01fb00b15ada863b19ee3aa8cf0e
Dale Calabia <33296464+Ravend-Biacala@users.noreply.github.com>
2019-03-27 10:07:19 +0000

    Create basics.r

---
+++ 2nd_year/PandS/r_labs/basics.r
@@ -0,0 +1,18 @@
+#1)	replicate a given dice (6 times)
+
+dice <- rep(1/6, times = 6)
+dice
+
+#2 What is the probability of getting less than 3
+sum(dice[1:2])
+
+#3  Header = TRUE!!!
+
+
+#6
+
+w = 210/300
+c = .210*.63 + .90*.75
+w+c
+
+
