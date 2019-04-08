5a131d204ec3afccc2b1ba06320f33dc99a656dc
Dale Calabia <33296464+Ravend-Biacala@users.noreply.github.com>
2019-04-08 13:24:44 +0100

    Create sample_exam.r

---
+++ 2nd_year/PandS/sample_exam.r
@@ -0,0 +1,33 @@
+#yes its possible to be true, because the 2% of cats that steal fish can be
+#responsible for 90% of fish thefts
+#
+#
+#
+only 2% of cats steal fish
+
+p(c|e)
+p(e|v)
+
+bayesian network
+
+directon of causality
+
+indipendany C and E
+
+P(C), P(E)
+whenever theres and arrow get the conditional prob
+this means its dependant on the parent
+4 possible combinations
+
+4a
+p(v|c^e)p(c)p(e)
+p(v|c^-e)p(c)p(-e)
+p(v|-c^e)p(-c)p(e)
+p(v|-c^-e)p(-c)p(-e)
+
+4b
+i)p(V) = sum of all prop in parts a = i + ii + iii + iv
+ii)p(V)andp(c) = part ii + part i 
+iii)p(c|v) = p(c^v)/p(v) ii/i
+iv) p(v)p(e) = i + iii
+v) iv+ / i
